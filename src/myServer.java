0c95ea21aa817831326bd24edecb611c13ac0e5a
Baltaj Bhandal <bbhandal5@my.bcit.ca>
2025-06-11 13:43:29 -0700

    Made more comments

--- src/myServer.java
+++ src/myServer.java
@@ -5,11 +5,8 @@
  * @version 1.0
  */
 
-    // Importing the java ServerSocket class
-
-    import java.io.BufferedReader;
+    // Importing the java ServerSocket and Socket class
     import java.io.IOException;
-    import java.io.InputStreamReader;
     import java.net.ServerSocket;
     import java.net.Socket;
     import java.util.Date;
@@ -19,13 +16,22 @@ class myServer {
 
      public static void main(String args[]) throws IOException {
 
+         // Create a server socket that listens on port 8080
          ServerSocket server = new ServerSocket(8080);
          System.out.println("Listening for connection on port 8080 ....");
+
+         // Infinite loop to continuously accept incoming client connections
          while (true) {
+
              try (Socket socket = server.accept()) {
-                 // Sending today's date to the client
+
+                 // Getting today's date
                  Date today = new Date();
+
+                 // Prepare a basic HTTP response with status code 200 OK and the current date
                  String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+
+                 // Send the response to the client
                  socket.getOutputStream()
                          .write(httpResponse.getBytes("UTF-8"));
              }
