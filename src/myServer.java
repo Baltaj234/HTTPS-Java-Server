cb2b00f9e33f617663b72b3b68f4ec34b1c44a90
Baltaj Bhandal <bbhandal5@my.bcit.ca>
2025-06-11 13:40:50 -0700

    Server now sends the current date to the client in the browser

--- src/myServer.java
+++ src/myServer.java
@@ -1,5 +1,6 @@
 /**
- * The class where the HTTP server will live
+ * Java program to create a simple HTTP Server to demonstrate
+ * how to use the ServerSocket and Socket class.
  * @author Baltaj Bhandal
  * @version 1.0
  */
@@ -7,29 +8,28 @@
     // Importing the java ServerSocket class
 
     import java.io.BufferedReader;
+    import java.io.IOException;
     import java.io.InputStreamReader;
     import java.net.ServerSocket;
     import java.net.Socket;
+    import java.util.Date;
 
- class myServer {
 
-     public static void main(String[] args) throws Exception {
-         final ServerSocket server = new ServerSocket(8080);
-         System.out.println("Listening for connection on port 8080 .....");
-         while(true){
+class myServer {
 
-             Socket clientSocket = server.accept();
-             InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-             BufferedReader reader = new BufferedReader(isr);
-             String line = reader.readLine();
-             while(!line.isEmpty()){
-                 System.out.println(line);
-                 line = reader.readLine();
+     public static void main(String args[]) throws IOException {
+
+         ServerSocket server = new ServerSocket(8080);
+         System.out.println("Listening for connection on port 8080 ....");
+         while (true) {
+             try (Socket socket = server.accept()) {
+                 // Sending today's date to the client
+                 Date today = new Date();
+                 String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                 socket.getOutputStream()
+                         .write(httpResponse.getBytes("UTF-8"));
              }
          }
-
      }
 
-
-
-}
+ }
